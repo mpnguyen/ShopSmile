c2778060623d6a029252505f72daa4bf52790eef
LeTanVu <letanvu.it@gmail.com>
2016-06-05 15:04:26 +0700

    Upload image to website

--- ShopSmile/js/controller.js
+++ ShopSmile/js/controller.js
@@ -12,47 +12,60 @@ app.controller('ShopSmileCtrl', function ($scope, $firebaseObject) {
 		$scope.listItems = $scope.data['products'][key];
 		console.log($scope.listItems);
 	};
-   
-    var provider = new firebase.auth.FacebookAuthProvider();
-    provider.addScope('public_profile');
-    
-    $scope.loginFacebook = function(){
-        firebase.auth().signInWithPopup(provider).then(function(result) {
-          // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-          var token = result.credential.accessToken;
-          // The signed-in user info.
-          var user = result.user;
-          // ...
-        }).catch(function(error) {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          // The email of the user's account used.
-          var email = error.email;
-          // The firebase.auth.AuthCredential type that was used.
-          var credential = error.credential;
-          // ...
-        });
-    };
-    
-    provider = new firebase.auth.GoogleAuthProvider();
-    provider.addScope('https://www.googleapis.com/auth/plus.login');
-    $scope.loginGoogle = function(){
-        firebase.auth().signInWithPopup(provider).then(function(result) {
-          // This gives you a Google Access Token. You can use it to access the Google API.
-          var token = result.credential.accessToken;
-          // The signed-in user info.
-          var user = result.user;
-          // ...
-        }).catch(function(error) {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          // The email of the user's account used.
-          var email = error.email;
-          // The firebase.auth.AuthCredential type that was used.
-          var credential = error.credential;
-          // ...
-        });
-    };
+
+	var provider = new firebase.auth.FacebookAuthProvider();
+	provider.addScope('public_profile');
+
+	$scope.loginFacebook = function () {
+		firebase.auth().signInWithPopup(provider).then(function (result) {
+			// This gives you a Facebook Access Token. You can use it to access the Facebook API.
+			var token = result.credential.accessToken;
+			// The signed-in user info.
+			var user = result.user;
+			// ...
+		}).catch(function (error) {
+			// Handle Errors here.
+			var errorCode = error.code;
+			var errorMessage = error.message;
+			// The email of the user's account used.
+			var email = error.email;
+			// The firebase.auth.AuthCredential type that was used.
+			var credential = error.credential;
+			// ...
+		});
+	};
+
+	provider = new firebase.auth.GoogleAuthProvider();
+	provider.addScope('https://www.googleapis.com/auth/plus.login');
+	$scope.loginGoogle = function () {
+		firebase.auth().signInWithPopup(provider).then(function (result) {
+			// This gives you a Google Access Token. You can use it to access the Google API.
+			var token = result.credential.accessToken;
+			// The signed-in user info.
+			var user = result.user;
+			// ...
+		}).catch(function (error) {
+			// Handle Errors here.
+			var errorCode = error.code;
+			var errorMessage = error.message;
+			// The email of the user's account used.
+			var email = error.email;
+			// The firebase.auth.AuthCredential type that was used.
+			var credential = error.credential;
+			// ...
+		});
+	};
+
+
+	$scope.imageUpload = function (element) {
+		var reader = new FileReader();
+		reader.onload = function (event) {
+				$scope.images = event.target.result
+				$scope.$apply()
+			}
+			// when the file is read it triggers the onload event above.
+		reader.readAsDataURL(element.files[0]);
+	};
+
+
 });
