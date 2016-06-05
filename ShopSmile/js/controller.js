78040afbd61a907dfc8a81ba5fb67e8d0e6c0309
1312168nchai <nchai.ctt@gmail.com>
2016-06-05 13:06:07 +0700

    sign in with facebook account

--- ShopSmile/js/controller.js
+++ ShopSmile/js/controller.js
@@ -11,5 +11,27 @@ app.controller('ShopSmileCtrl', function ($scope, $firebaseObject) {
 	$scope.loadPage = function (key) {
 
 	};
+   
+    var provider = new firebase.auth.FacebookAuthProvider();
+    provider.addScope('public_profile');
+    
+    $scope.loginFacebook = function(){
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+          // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+          var token = result.credential.accessToken;
+          // The signed-in user info.
+          var user = result.user;
+          // ...
+        }).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // ...
+        });
+    };
 
 });
